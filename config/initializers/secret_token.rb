11687982187c563534cef80bb7d3ee57c86824ea
mateuszpalyz@gmail.com <mateuszpalyz@gmail.com>
2014-01-12 19:31:42 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Przychodnia::Application.config.secret_key_base = '334d3cff2bc1f4ab8659946355435dea89bf110a14da2062dea2f09dd450df8e4a1d0e432ee6d86a72956e7e159c33680d2fa145ed290e3d3be287924b7e85e2'
