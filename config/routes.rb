57fa02779b93beee94667a55dc8f65ee62d3fbd4
mateuszpalyz@gmail.com <mateuszpalyz@gmail.com>
2014-01-12 23:09:41 +0100

    almost done with static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Przychodnia::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
+  root  'static_pages#home'
+  match '/help',    to: 'static_pages#help',   via: 'get'
+  match '/about',   to: 'static_pages#about',  via: 'get'
+  match '/policy',  to: 'static_pages#policy', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
