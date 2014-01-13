8179714072aec665cb3d04a3df166ddcd87ae67d
mateuszpalyz@gmail.com <mateuszpalyz@gmail.com>
2014-01-13 15:57:17 +0100

    finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
+  before_create :create_remember_token
+
   validates :name,      
   presence: { message: "Imię nie może być puste" }, 
   length: { maximum: 30, message: "Imię jest za długie, maksimum 30 znaków"} 
@@ -24,17 +26,27 @@ class User < ActiveRecord::Base
   uniqueness: { case_sensitive: false, message: "Konto o podanym adresie email już istnieje" }
   
   has_secure_password
-  #validates :password, 
-  #presence: { message: "Hasło nie może być puste"},
   
   validates_confirmation_of :password, message: "Hasła nie są takie same", if: :password_confirmation_required?
   validates_presence_of :password, message: "Hasło nie może być puste",  on: :create
   validates_length_of :password, minimum: 6, message: "Hasło za krótkie, minimum 6 znaków" 
   validates_presence_of :password_confirmation, message: "Potwierdz hasło", if: :password_confirmation_required?
 
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
   private
 
     def password_confirmation_required?
       password_confirmation && password.present?
+    end
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
     end
 end
