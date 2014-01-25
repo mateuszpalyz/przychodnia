768282fd382107fd0b354f62fe7e9958001d2ead
mateuszpalyz@gmail.com <mateuszpalyz@gmail.com>
2014-01-25 21:12:50 +0100

    b

--- db/migrate/20140125195121_change.rb
+++
@@ -1,6 +0,0 @@
-class Change < ActiveRecord::Migration
-  def change
-    remove_column :visits, :next_visit
-    add_column :visits, :next_visit, :datetime
-  end
-end
--- db/migrate/20140125200135_changing_colums_in_visits.rb
+++
@@ -1,7 +0,0 @@
-class ChangingColumsInVisits < ActiveRecord::Migration
-  def self.up
-    rename_column :visits, :next_visit, :date_entered_string
-    add_column :visits, :next_visit, :datetime
-    remove_column :visits, :date_entered_string
-  end
-end
